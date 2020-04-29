3284e920836c856896a9ee7caee7925517c550cd
CzB-kit <59529869+CzB-kit@users.noreply.github.com>
2020-04-29 12:26:30 +0200

    Add files via upload

---
+++ script/global.js
@@ -0,0 +1,24 @@
+// Script to open and close sidebar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
+// Change style of navbar on scroll
+window.onscroll = function() {
+    myFunction()
+};
+
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-my-color";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-my-color", "");
+    }
+}
